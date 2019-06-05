50fb894f5e4c1cd89bf9072148ef3ec0dcea05df
Alan S Mathew <alansmathew@Icloud.com>
2019-06-05 22:14:38 +0530

    ..

---
+++ dynamic msg/dynamic msg/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+
+   
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("Entered data is " & TextBox1.Text)
+        TextBox1.Clear()
+    End Sub
+End Class
---
+++ dynamic msg/dynamic msg/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("dynamic msg")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("dynamic msg")> 
+<Assembly: AssemblyCopyright("Copyright ©  2018")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b1dd62bb-6d9e-4288-97d2-8a0d8af7fad3")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
