dd8f80013e3055f6b01b97a37db69e059ef3829b
Alan S Mathew <alansmathew@Icloud.com>
2019-06-05 22:15:44 +0530

    ..

---
+++ FIND SUM OF N NUMBERS/FIND SUM OF N NUMBERS/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim num, sum As Integer
+        num = TextBox2.Text
+        While num >= 0
+            sum += num
+            num -= 1
+        End While
+        MsgBox("sum is " & sum)
+    End Sub
+End Class
---
+++ FIND SUM OF N NUMBERS/FIND SUM OF N NUMBERS/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("FIND SUM OF N NUMBERS")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("FIND SUM OF N NUMBERS")> 
+<Assembly: AssemblyCopyright("Copyright ©  2018")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d0efd826-52f6-4f11-9c8a-ff2edad92e99")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
