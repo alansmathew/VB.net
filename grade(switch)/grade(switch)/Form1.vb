c8c367e17aa6824c39da5cabdd0ecd1273f69e4d
Alan S Mathew <alansmathew@Icloud.com>
2019-06-06 10:15:37 +0530

    ..

---
+++ grade(switch)/grade(switch)/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim mark1, mark2, mark3, avg As Integer
+        mark1 = TextBox1.Text
+        mark2 = TextBox3.Text
+        mark3 = TextBox4.Text
+
+        avg = (mark1 + mark2 + mark2) \ 3
+
+        Select Case avg
+            Case 90 To 100
+                MsgBox("Grade is A+")
+            Case 80 To 89
+                MsgBox("Grade is A")
+            Case 70 To 79
+                MsgBox("Grade is B")
+            Case 60 To 69
+                MsgBox("Grade is C")
+            Case 50 To 59
+                MsgBox("Grade is P")
+            Case Is < 50
+                MsgBox("Reappere again !!")
+            Case Else
+                MsgBox("invalid input")
+        End Select
+        TextBox1.Clear()
+        TextBox3.Clear()
+        TextBox4.Clear()
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+
+    End Sub
+End Class
---
+++ grade(switch)/grade(switch)/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("grade(switch)")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("grade(switch)")> 
+<Assembly: AssemblyCopyright("Copyright ©  2018")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("5755ee8d-bf4a-4b80-9d45-57a8b6549611")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
