f05c1d97bd6866be8d95dce0a43c7d1e1ac61cf3
Alan S Mathew <alansmathew@Icloud.com>
2019-06-06 10:16:43 +0530

    ..

---
+++ greater number/greater number/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+
+  
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text > TextBox2.Text Then
+            MsgBox(TextBox1.Text & " is  greater")
+        ElseIf TextBox2.Text > TextBox1.Text Then
+            MsgBox(TextBox2.Text & " is  greater")
+        Else
+            MsgBox(TextBox1.Text & " & " & TextBox2.Text & " are equal !!")
+        End If
+
+        TextBox1.Clear()
+        TextBox2.Clear()
+    End Sub
+End Class
