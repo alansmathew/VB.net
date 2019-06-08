20bac89e1afc4849d16e2acec04f5ddd2a87f636
Alan S Mathew <alansmathew@Icloud.com>
2019-06-08 17:06:26 +0530

    ..

---
+++ largest of three numbers/largest of three numbers/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If (TextBox1.Text > TextBox2.Text > TextBox3.Text) Then
+            MsgBox(TextBox1.Text + " is Grater")
+        ElseIf (TextBox2.Text > TextBox3.Text > TextBox1.Text) Then
+            MsgBox(TextBox2.Text + " is Grater")
+        ElseIf (TextBox3.Text > TextBox2.Text > TextBox1.Text) Then
+            MsgBox(TextBox3.Text + " is Grater")
+        Else
+            MsgBox("All are equal")
+        End If
+    End Sub
+End Class
