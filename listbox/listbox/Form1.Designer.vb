4db75e108ca16af26df8dee664c958fedd8399cb
Alan S Mathew <43809019+alansmathew008@users.noreply.github.com>
2019-06-09 21:22:31 +0530

    Add files via upload

---
+++ listbox/listbox/Form1.Designer.vb
@@ -0,0 +1,147 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class add2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.add = New System.Windows.Forms.Button()
+        Me.delete = New System.Windows.Forms.Button()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ad = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Items.AddRange(New Object() {"First name", "Middle Name", "Last name", "Gender", "DOB", "Batch", "Rollno", "Father's Name", "Mother's Name", "Address", "Phone", "Mobile"})
+        Me.ListBox1.Location = New System.Drawing.Point(24, 57)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple
+        Me.ListBox1.Size = New System.Drawing.Size(183, 199)
+        Me.ListBox1.TabIndex = 0
+        '
+        'add
+        '
+        Me.add.Location = New System.Drawing.Point(213, 204)
+        Me.add.Name = "add"
+        Me.add.Size = New System.Drawing.Size(48, 23)
+        Me.add.TabIndex = 1
+        Me.add.Text = ">>"
+        Me.add.UseVisualStyleBackColor = True
+        '
+        'delete
+        '
+        Me.delete.Location = New System.Drawing.Point(213, 233)
+        Me.delete.Name = "delete"
+        Me.delete.Size = New System.Drawing.Size(48, 23)
+        Me.delete.TabIndex = 2
+        Me.delete.Text = "X"
+        Me.delete.UseVisualStyleBackColor = True
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(267, 57)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(190, 199)
+        Me.ListBox2.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(20, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(148, 24)
+        Me.Label1.TabIndex = 16
+        Me.Label1.Text = "Select the value "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(263, 20)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(148, 24)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "Selected values "
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(168, 280)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(140, 20)
+        Me.TextBox1.TabIndex = 19
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(91, 283)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 13)
+        Me.Label3.TabIndex = 20
+        Me.Label3.Text = "Add value "
+        '
+        'ad
+        '
+        Me.ad.Location = New System.Drawing.Point(324, 277)
+        Me.ad.Name = "ad"
+        Me.ad.Size = New System.Drawing.Size(48, 23)
+        Me.ad.TabIndex = 21
+        Me.ad.Text = "Add"
+        Me.ad.UseVisualStyleBackColor = True
+        '
+        'add2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(469, 335)
+        Me.Controls.Add(Me.ad)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.delete)
+        Me.Controls.Add(Me.add)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "add2"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents add As System.Windows.Forms.Button
+    Friend WithEvents delete As System.Windows.Forms.Button
+    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents ad As System.Windows.Forms.Button
+
+End Class
---
+++ listbox/listbox/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class add2
+
+
+    Private Sub add_Click(sender As Object, e As EventArgs) Handles add.Click
+        If ListBox1.SelectedItem = "" Then
+            MsgBox("Nothing selected!")
+        Else
+            ListBox2.Items.Add(ListBox1.SelectedItem)
+            ListBox1.Items.Remove(ListBox1.SelectedItem)
+        End If
+
+    End Sub
+
+    Private Sub delete_Click(sender As Object, e As EventArgs) Handles delete.Click
+        If ListBox2.SelectedItem <> "" Then
+            ListBox1.Items.Add(ListBox2.SelectedItem)
+            ListBox2.Items.Remove(ListBox2.SelectedItem)
+        ElseIf ListBox1.SelectedItem <> "" Then
+            ListBox1.Items.Remove(ListBox1.SelectedItem)
+
+        Else
+            MsgBox("Nothing selected!")
+        End If
+
+    End Sub
+
+
+    Private Sub ad_Click(sender As Object, e As EventArgs) Handles ad.Click
+        If TextBox1.Text = "" Then
+            MsgBox("Enter a value !")
+        Else
+
+            ListBox1.Items.Add(TextBox1.Text)
+            TextBox1.Clear()
+        End If
+    End Sub
+
+
+End Class
