e13734094d7593314768f3661a49fdf97690a306
Alan S Mathew <43809019+alansmathew@users.noreply.github.com>
2019-06-16 21:48:17 +0530

    Add files via upload

---
+++ question/question/Form1.Designer.vb
@@ -0,0 +1,351 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.q1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.q2 = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton6 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton7 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton8 = New System.Windows.Forms.RadioButton()
+        Me.q3 = New System.Windows.Forms.GroupBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.RadioButton9 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton10 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton11 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton12 = New System.Windows.Forms.RadioButton()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.q1.SuspendLayout()
+        Me.q2.SuspendLayout()
+        Me.q3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'q1
+        '
+        Me.q1.Controls.Add(Me.Label1)
+        Me.q1.Controls.Add(Me.RadioButton4)
+        Me.q1.Controls.Add(Me.RadioButton3)
+        Me.q1.Controls.Add(Me.RadioButton2)
+        Me.q1.Controls.Add(Me.RadioButton1)
+        Me.q1.Location = New System.Drawing.Point(36, 32)
+        Me.q1.Name = "q1"
+        Me.q1.Size = New System.Drawing.Size(503, 198)
+        Me.q1.TabIndex = 0
+        Me.q1.TabStop = False
+        Me.q1.Text = "Question 1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(32, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(80, 16)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "cut / paste"
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(307, 134)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(89, 17)
+        Me.RadioButton4.TabIndex = 3
+        Me.RadioButton4.Tag = "0"
+        Me.RadioButton4.Text = "shift c | shift v"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(63, 134)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(134, 17)
+        Me.RadioButton3.TabIndex = 2
+        Me.RadioButton3.Tag = "1"
+        Me.RadioButton3.Text = "cmd + c | cmd + shift v "
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(307, 94)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(98, 17)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.Tag = "0"
+        Me.RadioButton2.Text = "cmd + x | shift v"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(63, 94)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(77, 17)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.Tag = "0"
+        Me.RadioButton1.Text = "cmd + c | v"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(453, 259)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Next"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(389, 264)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(307, 264)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(13, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "1"
+        '
+        'q2
+        '
+        Me.q2.Controls.Add(Me.Label4)
+        Me.q2.Controls.Add(Me.RadioButton5)
+        Me.q2.Controls.Add(Me.RadioButton6)
+        Me.q2.Controls.Add(Me.RadioButton7)
+        Me.q2.Controls.Add(Me.RadioButton8)
+        Me.q2.Location = New System.Drawing.Point(36, 32)
+        Me.q2.Name = "q2"
+        Me.q2.Size = New System.Drawing.Size(503, 198)
+        Me.q2.TabIndex = 5
+        Me.q2.TabStop = False
+        Me.q2.Text = "Question 2"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(32, 40)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(46, 16)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "undo "
+        '
+        'RadioButton5
+        '
+        Me.RadioButton5.AutoSize = True
+        Me.RadioButton5.Location = New System.Drawing.Point(307, 134)
+        Me.RadioButton5.Name = "RadioButton5"
+        Me.RadioButton5.Size = New System.Drawing.Size(93, 17)
+        Me.RadioButton5.TabIndex = 3
+        Me.RadioButton5.TabStop = True
+        Me.RadioButton5.Tag = "0"
+        Me.RadioButton5.Text = "cmd + shift + z"
+        Me.RadioButton5.UseVisualStyleBackColor = True
+        '
+        'RadioButton6
+        '
+        Me.RadioButton6.AutoSize = True
+        Me.RadioButton6.Location = New System.Drawing.Point(63, 134)
+        Me.RadioButton6.Name = "RadioButton6"
+        Me.RadioButton6.Size = New System.Drawing.Size(63, 17)
+        Me.RadioButton6.TabIndex = 2
+        Me.RadioButton6.TabStop = True
+        Me.RadioButton6.Tag = "0"
+        Me.RadioButton6.Text = "cmd + d"
+        Me.RadioButton6.UseVisualStyleBackColor = True
+        '
+        'RadioButton7
+        '
+        Me.RadioButton7.AutoSize = True
+        Me.RadioButton7.Location = New System.Drawing.Point(307, 94)
+        Me.RadioButton7.Name = "RadioButton7"
+        Me.RadioButton7.Size = New System.Drawing.Size(63, 17)
+        Me.RadioButton7.TabIndex = 1
+        Me.RadioButton7.TabStop = True
+        Me.RadioButton7.Tag = "0"
+        Me.RadioButton7.Text = "cmd + q"
+        Me.RadioButton7.UseVisualStyleBackColor = True
+        '
+        'RadioButton8
+        '
+        Me.RadioButton8.AutoSize = True
+        Me.RadioButton8.Location = New System.Drawing.Point(63, 94)
+        Me.RadioButton8.Name = "RadioButton8"
+        Me.RadioButton8.Size = New System.Drawing.Size(65, 17)
+        Me.RadioButton8.TabIndex = 0
+        Me.RadioButton8.TabStop = True
+        Me.RadioButton8.Tag = "1"
+        Me.RadioButton8.Text = "cmd + z "
+        Me.RadioButton8.UseVisualStyleBackColor = True
+        '
+        'q3
+        '
+        Me.q3.Controls.Add(Me.Label5)
+        Me.q3.Controls.Add(Me.RadioButton9)
+        Me.q3.Controls.Add(Me.RadioButton10)
+        Me.q3.Controls.Add(Me.RadioButton11)
+        Me.q3.Controls.Add(Me.RadioButton12)
+        Me.q3.Location = New System.Drawing.Point(36, 32)
+        Me.q3.Name = "q3"
+        Me.q3.Size = New System.Drawing.Size(503, 198)
+        Me.q3.TabIndex = 5
+        Me.q3.TabStop = False
+        Me.q3.Text = "Question 3"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(32, 40)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(46, 16)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "save "
+        '
+        'RadioButton9
+        '
+        Me.RadioButton9.AutoSize = True
+        Me.RadioButton9.Location = New System.Drawing.Point(307, 134)
+        Me.RadioButton9.Name = "RadioButton9"
+        Me.RadioButton9.Size = New System.Drawing.Size(86, 17)
+        Me.RadioButton9.TabIndex = 3
+        Me.RadioButton9.Tag = "0"
+        Me.RadioButton9.Text = "cmd + space"
+        Me.RadioButton9.UseVisualStyleBackColor = True
+        '
+        'RadioButton10
+        '
+        Me.RadioButton10.AutoSize = True
+        Me.RadioButton10.Location = New System.Drawing.Point(63, 134)
+        Me.RadioButton10.Name = "RadioButton10"
+        Me.RadioButton10.Size = New System.Drawing.Size(65, 17)
+        Me.RadioButton10.TabIndex = 2
+        Me.RadioButton10.Tag = "1"
+        Me.RadioButton10.Text = "cmd + s "
+        Me.RadioButton10.UseVisualStyleBackColor = True
+        '
+        'RadioButton11
+        '
+        Me.RadioButton11.AutoSize = True
+        Me.RadioButton11.Location = New System.Drawing.Point(307, 94)
+        Me.RadioButton11.Name = "RadioButton11"
+        Me.RadioButton11.Size = New System.Drawing.Size(63, 17)
+        Me.RadioButton11.TabIndex = 1
+        Me.RadioButton11.Tag = "0"
+        Me.RadioButton11.Text = "cmd + e"
+        Me.RadioButton11.UseVisualStyleBackColor = True
+        '
+        'RadioButton12
+        '
+        Me.RadioButton12.AutoSize = True
+        Me.RadioButton12.Location = New System.Drawing.Point(63, 94)
+        Me.RadioButton12.Name = "RadioButton12"
+        Me.RadioButton12.Size = New System.Drawing.Size(62, 17)
+        Me.RadioButton12.TabIndex = 0
+        Me.RadioButton12.Tag = "0"
+        Me.RadioButton12.Text = "cmd + x"
+        Me.RadioButton12.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(316, 264)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(18, 13)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "/3"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(561, 299)
+        Me.Controls.Add(Me.q3)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.q2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.q1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.q1.ResumeLayout(False)
+        Me.q1.PerformLayout()
+        Me.q2.ResumeLayout(False)
+        Me.q2.PerformLayout()
+        Me.q3.ResumeLayout(False)
+        Me.q3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents q1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents RadioButton4 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents q2 As System.Windows.Forms.GroupBox
+    Friend WithEvents q3 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents RadioButton9 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton10 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton11 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton12 As System.Windows.Forms.RadioButton
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents RadioButton5 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton6 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton7 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton8 As System.Windows.Forms.RadioButton
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+
+End Class
---
+++ question/question/Form1.vb
@@ -0,0 +1,59 @@
+﻿Public Class Form1
+    Dim score As Integer = 0
+    Dim i As Integer = 1
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If Label2.Text > 0 Then
+            Label2.Text = Label2.Text - 1
+        Else
+            Button1.PerformClick()
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        q1.Visible = True
+        q2.Visible = False
+        q3.Visible = False
+        Timer1.Start()
+        Label2.Text = 10
+        '   RadioButton1.Checked = False
+        ' RadioButton2.Checked = False
+        ' RadioButton3.Checked = False
+        ' RadioButton4.Checked = False
+        score = 0
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If i = 1 Then
+            If RadioButton3.Checked = True Then
+                score += RadioButton3.Tag
+            End If
+            q1.Visible = False
+            q2.Visible = True
+            Label2.Text = 10
+            i += 1
+            Label3.Text = i
+
+        ElseIf i = 2 Then
+            If RadioButton8.Checked = True Then
+                score += RadioButton8.Tag
+            End If
+            Label2.Text = 10
+            q2.Visible = False
+            q3.Visible = True
+            i += 1
+            Label3.Text = i
+        Else
+            If RadioButton10.Checked = True Then
+                score += RadioButton10.Tag
+            End If
+            Timer1.Stop()
+            q3.Enabled = False
+            MsgBox("Total score is " & score)
+        End If
+
+    End Sub
+
+End Class
